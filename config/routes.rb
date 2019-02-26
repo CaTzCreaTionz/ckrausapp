911b243017ec9bfa62d404d3cb747fd486498270
CaTzCreaTionz <ckraus0089@gmail.com>
2019-02-26 21:22:43 +0100

    Comments Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :products do
+   resources :comments
+  end
+
   resources :users
-  resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
+  
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
   get 'simple_pages/landing_page'
   root 'simple_pages#index'
-  resources :orders, only: [:index, :show, :create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'simple_pages/thank_you'
 end
