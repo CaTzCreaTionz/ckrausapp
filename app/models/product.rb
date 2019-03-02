3f4344745f31cc72ca8b7391200b45edfecd639e
CaTzCreaTionz <ckraus0089@gmail.com>
2019-03-02 17:49:30 +0100

    5.9 Validation and Pagination

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :comments
 
+  validates :name, presence: true
+
   def self.search(search_term)
     if Rails.env.production? # use "ilike" matching operator for postgres to ignore upper- and lower-case
       Product.where("name ilike ?", "%#{search_term}%")
