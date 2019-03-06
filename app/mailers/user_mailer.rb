7221962fa01b330359a5a763dc8ee6b5f164bb77
CaTzCreaTionz <ckraus0089@gmail.com>
2019-03-06 20:29:58 +0100

    5.11 More validations, add UserRegistrationsController, more css

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,9 +2,16 @@ class UserMailer < ApplicationMailer
   default from: "catz@catz-creationz.com"
 
   def contact_form(email, name, message)
-  @message = message
+    @message = message
     mail(from: email,
          to: 'ckraus0089@gmail.com',
          subject: "A new contact form message from #{name}")
+  end
+
+  def welcome
+    @user = params[:user]
+    @appname = "CaTz-CreaTionz"
+    mail(to: @user.email,
+       subject: "Welcome to #{@appname}!")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, controllers: { registrations: "user_registrations" }
 
   resources :products do
    resources :comments
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :users
   resources :orders, only: [:index, :show, :create, :destroy]
-  
+
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
